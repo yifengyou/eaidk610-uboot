c5267692a10a9bf80d63880f67ee6f2fb3dd57bc
yifengyou <842056007@qq.com>
2023-04-06 22:33:47 +0800

    add gitignore and eaidk610.sh

---
+++ eaidk610.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export PATH=/data/armbian/armbian.git/cache/toolchain/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin:$PATH
+#export PATH=/data/armbian/kernel_and_uboot/gcc-arm-11.2-2022.02-x86_64-aarch64-none-none-linux-gnu/bin:$PATH
+#export PATH=/data/armbian/armbian.git/cache/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-none-linux-gnu:$PATH
+
+make distclean
+
+make ARCH=arm  CROSS_COMPILE="aarch64-linux-gnu-" rk3399_linux_defconfig
+#make ARCH=arm  CROSS_COMPILE="aarch64-none-linux-gnu-" rk3399_linux_defconfig
+echo "make rk3399_linux_defconfig done! [$?]"
+
+echo    "******************************"
+echo    "*     Make AArch64 Uboot     *"
+echo    "******************************"
+#make ARCH=arm V=1 CROSS_COMPILE="aarch64-none-linux-gnu-" ARCHV=aarch64 --jobs=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
+#make ARCH=arm V=1 CROSS_COMPILE="aarch64-none-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
+make ARCH=arm V=1 CROSS_COMPILE="aarch64-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
+echo "make done! [$?]"
+
+exit 0
