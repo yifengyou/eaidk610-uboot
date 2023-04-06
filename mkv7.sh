c9e564b3ac59a2d174d3acbfdf577c7ad6c4997b
yifengyou <842056007@qq.com>
2023-04-06 23:47:11 +0800

    delete : remove mkv7.sh no need to build 32bit

--- mkv7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo    "******************************"
-echo    "*     Make AArch32 Uboot     *"
-echo    "******************************"
-make --jobs=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
-
