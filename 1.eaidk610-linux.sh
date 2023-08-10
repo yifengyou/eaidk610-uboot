453a395a0f01b9821618c3b35dabfcd49745bf9d
yifengyou <842056007@qq.com>
2023-08-10 22:23:47 +0800

    modify build dir

--- 1.eaidk610-linux.sh
+++ 1.eaidk610-linux.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+set -e
+
 export PATH=`realpath ../gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin`:$PATH
 echo $PATH
+
+CURRENTDIR=`pwd`
+echo "CURRENTDIR: ${CURRENTDIR}"
+BUILDDIR=${CURRENTDIR}-build
+echo "BUILDDIR: ${BUILDDIR}"
+if [ -d ${BUILDDIR} ]; then
+	rm -rf ${BUILDDIR}
+fi
+mkdir -p ${BUILDDIR}
 
 echo    "******************************"
 echo    "*     Clean Uboot Config     *"
@@ -12,14 +23,13 @@ echo " * make distclean done! [$?]"
 echo    "******************************"
 echo    "*     Make Uboot Config      *"
 echo    "******************************"
-make ARCH=arm  O=build CROSS_COMPILE="aarch64-linux-gnu-" rk3399_linux_defconfig
+make O=${BUILDDIR} ARCH=arm  CROSS_COMPILE="aarch64-linux-gnu-" rk3399_linux_defconfig
 echo " * make rk3399_linux_defconfig done! [$?]"
 
 echo    "******************************"
 echo    "*     Make AArch64 Uboot     *"
 echo    "******************************"
-make ARCH=arm V=1 O=build CROSS_COMPILE="aarch64-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
+make O=${BUILDDIR} ARCH=arm V=1 CROSS_COMPILE="aarch64-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
 echo " * make done! [$?]"
 
 echo " * All done!"
-exit 0
--- 2.eaidk610-android.sh
+++ 2.eaidk610-android.sh
@@ -3,22 +3,31 @@
 export PATH=`realpath ../gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin`:$PATH
 echo $PATH
 
+CURRENTDIR=`pwd`
+echo "CURRENTDIR: ${CURRENTDIR}"
+BUILDDIR=${CURRENTDIR}-build
+echo "BUILDDIR: ${BUILDDIR}"
+if [ -d ${BUILDDIR} ]; then
+    rm -rf ${BUILDDIR}
+fi
+mkdir -p ${BUILDDIR}
+
 echo    "******************************"
 echo    "*     Clean Uboot Config     *"
 echo    "******************************"
-make distclean
+make O=${BUILDDIR} distclean
 echo "make distclean done! [$?]"
 
 echo    "******************************"
 echo    "*     Make Uboot Config      *"
 echo    "******************************"
-make ARCH=arm  CROSS_COMPILE="aarch64-linux-gnu-" rk3399_defconfig
+make O=${BUILDDIR} ARCH=arm  CROSS_COMPILE="aarch64-linux-gnu-" rk3399_defconfig
 echo "make rk3399_defconfig done! [$?]"
 
 echo    "******************************"
 echo    "*     Make AArch64 Uboot     *"
 echo    "******************************"
-make ARCH=arm V=1 CROSS_COMPILE="aarch64-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
+make O=${BUILDDIR} ARCH=arm V=1 CROSS_COMPILE="aarch64-linux-gnu-" ARCHV=aarch64 --jobs=`nproc`
 echo "make done! [$?]"
 
 exit 0
